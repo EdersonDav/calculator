6261a481542ac6d156436bda0e7c5483c3a81e8e
Ederson Davi <silva.edersonqueiroz@gmail.com>
2020-04-24 21:33:15 -0300

    First Commit

---
+++ javascript/script.js
@@ -0,0 +1,48 @@
+//Select number
+var seven = document.querySelector('#seven')
+var eight = document.querySelector('#eight')
+var nine = document.querySelector('#nine')
+var four = document.querySelector('#four')
+var five = document.querySelector('#five')
+var six = document.querySelector('#six')
+var one = document.querySelector('#one')
+var two = document.querySelector('#two')
+var three = document.querySelector('#three')
+var zero = document.querySelector('#zero')
+
+//Select itens
+var dot = document.querySelector('#dot')
+var com = document.querySelector('#com')
+var ad = document.querySelector('#ad')
+var sub = document.querySelector('#sub')
+var clear = document.querySelector('#clear')
+var divs = document.querySelector('#divs')
+var back = document.querySelector('#back')
+var mod = document.querySelector('#mod')
+var mult = document.querySelector('#mult')
+var equal = document.querySelector('#equal')
+
+//Event numbers
+seven.addEventListener('click', function (){console.log(seven.value)})
+eight.addEventListener('click', function (){console.log(eight.value)})
+nine.addEventListener('click', function (){console.log(nine.value)})
+four.addEventListener('click', function (){console.log(four.value)})
+five.addEventListener('click', function (){console.log(five.value)})
+six.addEventListener('click', function (){console.log(six.value)})
+one.addEventListener('click', function (){console.log(one.value)})
+two.addEventListener('click', function (){console.log(two.value)})
+three.addEventListener('click', function (){console.log(three.value)})
+zero.addEventListener('click', function (){console.log(zero.value)})
+
+//Event itens
+dot.addEventListener('click', function (){console.log(dot.value)})
+com.addEventListener('click', function (){console.log(com.value)})
+ad.addEventListener('click', function (){console.log(ad.value)})
+sub.addEventListener('click', function (){console.log(sub.value)})
+clear.addEventListener('click', function (){console.log(clear.value)})
+divs.addEventListener('click', function (){console.log(divs .value)})
+back.addEventListener('click', function (){console.log(back .value)})
+mod.addEventListener('click', function (){console.log(mod.value)})
+mult.addEventListener('click', function (){console.log(mult .value)})
+equal.addEventListener('click', function (){console.log(equal.value)})
+
